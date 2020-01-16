8cf4f56f125b9a7230265db801cfe5ec5c81ed4e
Rogerio Oliveira <rogerio@piloti.com.br>
2020-01-16 20:08:25 -0300

    Include

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
        
         <?php include 'http://www.coletivodoporto.com.br/inc/topo.php'; ?>
 
-        <?php include("http://www.coletivodoporto.com.br/inc/banner_home.php") ?>
+        <?php include 'http://www.coletivodoporto.com.br/inc/banner_home.php'; ?>
 
         <div class="row services home_space ">
             <!-- Service 1 -->
@@ -62,7 +62,7 @@
 
         </div>
         
-        <?php include("http://www.coletivodoporto.com.br/inc/rodape.php") ?>
+        <?php include 'http://www.coletivodoporto.com.br/inc/rodape.php'; ?>
 
         <script>
             document.write('<script src=' +
