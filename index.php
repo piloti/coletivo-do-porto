de4a8e4d45e5c8f8d49eff9eb49713e47d38cae8
Rogerio Oliveira <rogerio@piloti.com.br>
2020-01-16 20:11:35 -0300

    add top header

--- index.php
+++ index.php
@@ -36,7 +36,51 @@
     </head>
     <body data-smooth-scrolling="1" id="homeActive">
        
-        <?php include 'http://www.coletivodoporto.com.br/inc/topo.php'; ?>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-22079211-4', 'coletivodoporto.com.br');
+        ga('send', 'pageview');
+
+    </script>
+
+        <div id="header">
+            <div class="row">
+
+                <!-- Logo -->
+                <div class="large-6 columns logoHolder">
+                    <a href="index.php" title="Welcome to our Homepage"><img src="img/logo.png" alt="Coletivo do Porto Logo"></a>
+                </div>
+                
+                <!-- Navigation -->
+                <div class="large-6 columns navHolder hide-for-small nav-div">
+                    <ul id="nav">
+                        <li class="nav_home"><a class="margin_home" href="index.php"></a></li>
+                        <li class="nav_somos" ><a href="nos_somos.php" class="animation">NÓS SOMOS</a></li>
+                        <li class="nav_coletivo"><a href="o_coletivo.php" class="animation">O COLETIVO</a></li>
+                        <li class="nav_porto"><a href="do_porto.php" class="animation">DO PORTO</a></li>
+                        <li class="nav_contato"><a class="margin_contato" href="contato.php"></a></li>
+                    </ul>
+                </div>
+
+
+                <!-- Select Navigation for Small Screens -->
+                <select id="smallnav" class="hide-for-large-up show-for-small"> 
+                    <option value="#" selected="selected">Menu</option> 
+                    
+                    <option value="index.php">HOME</option> 
+                    <option value="nos_somos.php">NÓS SOMOS</option> 
+                    <option value="o_coletivo.php">O COLETIVO</option> 
+                    <option value="do_porto.php">DO PORTO</option> 
+                    <option value="contato.php">CONTATO</option> 
+                </select>
+
+            </div>
+        </div>
+        <?php //include 'http://www.coletivodoporto.com.br/inc/topo.php'; ?>
 
         <?php include 'http://www.coletivodoporto.com.br/inc/banner_home.php'; ?>
 
@@ -44,7 +88,7 @@
             <!-- Service 1 -->
 
             <div class="large-4 columns serviceHolder section_featured_texts animationStart">
-                <div class="titulo1">Apenas teste Somos um pool de três empresas do setor criativo,<span> que compartilham espaço, ideias e experiências para criar soluções integradas em comunicação, marketing, desenvolvimento digital e projetos relacionados a inovação e design.<br> A formação multidisciplinar das equipes do <strong>Coletivo do Porto</strong> está entre os diferenciais que levam ao desenvolvimento de trabalhos de excelência em diversos setores de atuação.<br> A interação em um mesmo ambiente traz simbiose e sincronismo entre as diferentes equipes e garante projetos realmente coletivos.</span></div>
+                <div class="titulo1">Somos um pool de três empresas do setor criativo,<span> que compartilham espaço, ideias e experiências para criar soluções integradas em comunicação, marketing, desenvolvimento digital e projetos relacionados a inovação e design.<br> A formação multidisciplinar das equipes do <strong>Coletivo do Porto</strong> está entre os diferenciais que levam ao desenvolvimento de trabalhos de excelência em diversos setores de atuação.<br> A interação em um mesmo ambiente traz simbiose e sincronismo entre as diferentes equipes e garante projetos realmente coletivos.</span></div>
                
             </div>
             
